f549e0ef639762bf8156c309ce515bb8773153c3
Administrator <admin@example.com>
2026-02-01 22:26:34 +0800

    chore: sync UI version to 3.0.3

--- js/auto_matcher.js
+++ js/auto_matcher.js
@@ -1,7 +1,7 @@
 import { app } from "../../scripts/app.js";
 import { api } from "../../scripts/api.js";
 
-const VERSION = "3.0.1";
+const VERSION = "3.0.3";
 
 app.registerExtension({
     name: "Comfy.AutoModelMatcher",
